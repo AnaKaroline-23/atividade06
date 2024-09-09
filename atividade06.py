06b6f751d98e2e27eb9ffb0bf3b189d438fca86c
AnaKaroline-23 <anakarolinesoraes.23@gmail.com>
2024-09-09 09:09:39 -0300

    atividade06

---
+++ atividade06.py
@@ -0,0 +1,8 @@
+# Crie um programa que receba dois nomes (primeiro nome e sobrenome) e exiba o nome completo.
+nome = input( " nome " )
+sobrenome = input( " sobrenome " )
+soma = ( nome + sobrenome )
+print ( nome + " " + sobrenome )
+
+
+
